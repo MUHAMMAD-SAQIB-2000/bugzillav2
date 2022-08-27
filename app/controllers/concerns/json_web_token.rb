895457de3d805503f1999b75bcc450be31056356
MUHAMMAD-SAQIB-2000 <saqibmakhan@gmail.com>
2022-08-27 22:40:50 +0500

    fixed minor issues

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -4,7 +4,7 @@ module JsonWebToken
   extend ActiveSupport::Concern
   SECRET_KEY = Rails.application.secret_key_base
 
-  def jwt_encode(payload, exp = 5.minute.from_now)
+  def jwt_encode(payload, exp = 60.minute.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
